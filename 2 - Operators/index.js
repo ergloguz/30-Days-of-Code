05c0a885ec9e67fc3f61c9fbd72b4bd5836b7358
Oğuz Ergül <60192962+ergloguz@users.noreply.github.com>
2020-12-21 15:08:31 +0300

    Day 2 completed.

---
+++ 2 - Operators/index.js
@@ -0,0 +1,11 @@
+function solve(meal_cost, tip_percent, tax_percent) {}
+
+function main() {
+    const meal_cost = parseFloat(readLine());
+
+    const tip_percent = parseInt(readLine(), 10);
+
+    const tax_percent = parseInt(readLine(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
